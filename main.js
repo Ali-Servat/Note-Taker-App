750af74eb5a7a08df42dea8f06fb8568a9e89070
AliServat <aliservat20@gmail.com>
2021-10-22 11:29:24 +0330

    second attempt to make the work complete

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const textarea = document.querySelector('#note');
 const addBtn = document.querySelector('button');
 const list = document.querySelector('ul');
+const body = document.querySelector('body');
 
 addBtn.addEventListener('click',(e)=>{
     e.preventDefault();
@@ -13,5 +14,33 @@ addBtn.addEventListener('click',(e)=>{
     const liPara = document.createElement('p');
     liPara.innerHTML = textarea.value;
     li.appendChild(liPara);
+    const viewAllBtn = document.createElement('button');
+    viewAllBtn.innerHTML = 'View All';
+    li.appendChild(viewAllBtn);
+    const deleteBtn = document.createElement('button');
+    deleteBtn.innerHTML = 'X';
+    li.appendChild(deleteBtn);
     list.appendChild(li);
+
+    viewAllBtn.addEventListener('click',()=>{
+        const parentDiv = document.createElement('div');
+    parentDiv.classList = 'parentDiv';
+    body.appendChild(parentDiv);
+    const childDiv = document.createElement('div');
+    childDiv.classList = 'childDiv';
+    childDiv.innerHTML = viewAllBtn.parentNode.childNodes[1].textContent;
+    parentDiv.appendChild(childDiv);
+    const deleteBtn = document.createElement('button');
+    deleteBtn.innerHTML = 'Close';
+    childDiv.appendChild(deleteBtn);
+    deleteBtn.addEventListener('click',(e)=>{
+    e.preventDefault();
+    
+    deleteBtn.parentNode.parentNode.parentNode.removeChild(deleteBtn.parentNode.parentNode);
+    })
+    });
 })
+
+const DisplayMessage = function(){
+    
+}
